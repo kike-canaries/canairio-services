cf638a1575e0a58e7e6014eed383e1d2c95b4ce6
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2020-09-29 16:05:22 -0500

    Django REST Swagger: deprecated (2019-06-04)

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
+    'drf_yasg',
     'admin_influxdb',
     'canairio_user',
     'canairio_station',
--- api/api/urls.py
+++ api/api/urls.py
@@ -18,25 +18,32 @@ from django.urls import include, path
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
-from rest_framework.schemas import get_schema_view
-from django.views.generic import TemplateView
 
-openapi_schema = get_schema_view(
-    title="CanAirIO Services",
-    version="1.0.0",
-    description="API for all CanAirIO services",
-    #authentication_classes=[],
-    #permission_classes=[],
-)
+from django.conf.urls import url
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-doc_schema = TemplateView.as_view(
-    template_name='doc.html',
-    extra_context={'schema_url': 'openapi-schema'}
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('openapi/', openapi_schema, name='openapi-schema'),
-    path('doc/', doc_schema, name='doc-schema'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger',
+                                           cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc',
+                                         cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/token-auth/', obtain_jwt_token),
     path('api/token-refresh/', refresh_jwt_token),
