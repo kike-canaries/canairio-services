3329c9362a9813620079e5ecaa22f88680e30cf7
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2020-09-29 11:42:26 -0500

    Swagger OpenAPI Documentation

--- api/api/urls.py
+++ api/api/urls.py
@@ -18,8 +18,25 @@ from django.urls import include, path
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+
+openapi_schema = get_schema_view(
+    title="CanAirIO Services",
+    version="1.0.0",
+    description="API for all CanAirIO services",
+    #authentication_classes=[],
+    #permission_classes=[],
+)
+
+doc_schema = TemplateView.as_view(
+    template_name='doc.html',
+    extra_context={'schema_url': 'openapi-schema'}
+)
 
 urlpatterns = [
+    path('openapi/', openapi_schema, name='openapi-schema'),
+    path('doc/', doc_schema, name='doc-schema'),
     path('admin/', admin.site.urls),
     path('api/token-auth/', obtain_jwt_token),
     path('api/token-refresh/', refresh_jwt_token),
